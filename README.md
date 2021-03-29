90df07346f881d887292352cc43226e1213fa55b
xhiny <axhiny@163.com>
2021-03-29 14:04:16 +0800

    支持展示接口返回的内容

--- README.md
+++ README.md
@@ -52,3 +52,36 @@ composer require quansitech/qscmf-columntype-modal
     // ListBuilder对应列配置
     ->addTableColumn('nick_name', '用户名', 'modal', $this->genModelHtml(), false, '点击查看更多信息')
 ```
++ 通过value设置api_url，支持展示接口返回的内容
+```php
+// 接口返回JSON数据格式
+// 若数据正常则设置status为1，否则为0
+// 将需要返回的内容赋值给info
+public function genModelHtml($id){
+    $info = D('User')->getOne($id); 
+    
+    if (!$info){
+        $this->ajaxReturn(['status' => 0, 'info' => 'not found']);
+    }
+    $builder = new FormBuilder();
+    $builder
+        ->addFormItem('title', 'text', '标题')
+        ->addFormItem('summary', 'textarea', '简介')
+        ->addFormItem('cover', 'picture', '封面', '尺寸为214*250px', ['width' => 214, 'height' => 250])
+        ->setFormData($info)
+        ->setShowBtn(false)
+        ->setReadOnly(true);
+
+    $this->ajaxReturn(['status' => 1, 'info' => $builder->display(true)]);
+}
+
+ $option = [
+    'api_url' => U('genModelHtml', ['id'=>'__id__'], true, true),
+    'width' => '50%',
+    'height' => '50%'
+];
+
+// ListBuilder对应列配置
+->addTableColumn('nick_name', '用户名', 'modal', $option, false, '点击查看更多信息');
+
+```
--- src/ColumnType/Modal/Modal.php
+++ src/ColumnType/Modal/Modal.php
@@ -8,27 +8,74 @@ use Think\View;
 
 class Modal extends ColumnType
 {
+    protected $content = null;
+    protected $width = null;
+    protected $height = null;
+    protected $api_url = null;
+
     public function build(array &$option, array $data, $listBuilder)
     {
+        $this->init($option['value']);
+
         $view = new View();
+        $view->assign('width', $this->width);
+        $view->assign('height', $this->height);
+        $view->assign('content', $this->content);
+        $view->assign('api_url', $this->api_url);
+
         $view->assign('gid', Str::uuid());
         $view->assign('item', $option);
         $view->assign('value', $data[$option['name']]);
 
-        $view = $this->parseOptionValue($option['value'], $view);
-
         return $view->fetch(__DIR__ . '/modal.html');
     }
 
-    private function parseOptionValue($value, $view){
+    /**
+     * @param null $content
+     */
+    protected function setContent($content): void
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @param null $api_url
+     */
+    protected function setApiUrl($api_url): void
+    {
+        $this->api_url = $api_url;
+    }
+
+    /**
+     * @param null $width
+     */
+    protected function setWidth($width): void
+    {
+        $this->width = $width;
+    }
+
+    /**
+     * @param null $height
+     */
+    protected function setHeight($height): void
+    {
+        $this->height = $height;
+    }
+
+    private function init($value){
+        $value['width'] && $this->setWidth($value['width']);
+        $value['height'] && $this->setHeight($value['height']);
+        $value['api_url'] && $this->setApiUrl($value['api_url']);
+        !$this->api_url && $this->parseContent($value);
+    }
+
+    private function parseContent($value){
         if (is_array($value)){
             $content = $value['content'];
         }else{
             $content = $value;
         }
 
-        $view->assign('content', $content);
-
-        return $view;
+        $this->setContent($content);
     }
 }
